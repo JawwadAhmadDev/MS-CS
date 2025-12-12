2cd378c822f2114e4e9d32abd7bcc8f410c58d1c
Jawwad Ahmad <jawwadahmad.edu@gmail.com>
2025-12-12 16:21:46 +0500

    further data added

--- Deep Learning/self/Preprocessing.py
+++ Deep Learning/self/Preprocessing.py
@@ -7,7 +7,7 @@ X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
 from sklearn.impute import SimpleImputer
-imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = SimpleImputer(strategy = 'mean')
 imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
@@ -32,4 +32,14 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test) 
+X_test = sc_X.transform(X_test)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+y_pred = regressor.predict(X_test)
+
+
+
+plt.scatter(X_train, y_train, color='red')
